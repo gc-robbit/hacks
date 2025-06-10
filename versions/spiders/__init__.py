987a7e423a24ac2046c4caa469114c748be64805
Robert Dahlström <robert@dahlstrom.email>
2025-06-10 21:39:22 +0200

    chore: add type hints to methods

--- versions/spiders/__init__.py
+++ versions/spiders/__init__.py
@@ -14,10 +14,10 @@ Various 'spiders' that know how to retrieve version information from different s
 """
 
 
-def _beautify_version(version, beautify):
+def _beautify_version(version, beautify) -> str:
     """
     Attempts to return the numerical part of a version by stripping away common prefix and postfix notation
-    E.g. release- or v or x.y.z-just-some-annoying-text
+    Strings stripped can be release-, v or x.y.z-just-some-annoying-text
     """
     if not beautify:
         return version
@@ -38,7 +38,7 @@ def _contains_version(candidate):
     return re.match(r'[\d+.]+', beautified)
 
 
-def _get_version_from_metadata_label(yaml_data):
+def _get_version_from_metadata_label(yaml_data) -> str:
     if 'labels' not in yaml_data['metadata']:
         raise ValueError('Key: labels not found in metadata for {name}'.format(name=yaml_data['metadata']['name']))
 
@@ -60,7 +60,7 @@ class AbstractSpider(abc.ABC):
     Base class for all spiders
     """
     @abc.abstractmethod
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         pass
 
 
@@ -72,7 +72,7 @@ class AlpinePackageSpider(AbstractSpider):
         url = "https://pkgs.alpinelinux.org/packages?name={name}&branch={branch}&arch=x86_64"
         self.url = url.format(name=name, branch=branch)
 
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         response = requests.get(self.url)
         response.raise_for_status()
         tree = html.fromstring(response.content)
@@ -84,12 +84,11 @@ class BitbucketReleaseSpider(AbstractSpider):
     """
     Retrieve version from Bitbucket tags. Assumes x.y.z tags are used!
     """
-    def __init__(self, owner, repository):
-        api = "https://api.bitbucket.org/2.0/repositories/{owner}/{repository}/refs/tags?sort=-name&pagelen=100"
-        self.url = api.format(owner=owner, repository=repository)
+    def __init__(self, owner: str, repository: str) -> None:
+        self.url = f'https://api.bitbucket.org/2.0/repositories/{owner}/{repository}/refs/tags?sort=-name&pagelen=100'
         self.version_pattern = re.compile(r'^\d+.\d+.\d+$')
 
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         response = requests.get(self.url)
         response.raise_for_status()
         data = response.json()
@@ -102,11 +101,11 @@ class DockerfileSpider(AbstractSpider):
     """
     Retrieve version from a local Dockerfile (parse the Dockerfile FROM entry)
     """
-    def __init__(self, path):
+    def __init__(self, path: str) -> None:
         self.path = path
         self.re = re.compile(r'^FROM .*:v?(\d+.*)$')
 
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         """
         Expects a Dockerfile with this format:
         FROM jada/jada:1.2.3
@@ -125,14 +124,13 @@ class DockerHubSpider(AbstractSpider):
     Retrieve version from Docker hub tags list, sorting all versions using natsort to find the highest one.
     Assumes the tag names only contains versions!
     """
-    def __init__(self, owner, name):
+    def __init__(self, owner: str, name: str) -> None:
         self.owner = owner
         self.name = name
-        api = "https://registry.hub.docker.com/v2/repositories/{owner}/{name}/tags?page_size=100"
-        self.url = api.format(owner=self.owner, name=self.name)
+        self.url = f'https://registry.hub.docker.com/v2/repositories/{owner}/{name}/tags?page_size=100'
         self.version_pattern = re.compile(r'^\d+.\d+.\d+(-\d)?$')
 
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         response = requests.get(self.url)
         response.raise_for_status()
 
@@ -146,12 +144,11 @@ class GithubLatestReleaseSpider(AbstractSpider):
     """
     Retrieve version for the latest release of a GitHub project using the GitHub API
     """
-    def __init__(self, owner, repository):
-        api = 'https://api.github.com/repos/{owner}/{repository}/releases/latest'
-        self.url = api.format(owner=owner, repository=repository)
+    def __init__(self, owner: str, repository: str) -> None:
+        self.url = f'https://api.github.com/repos/{owner}/{repository}/releases/latest'
 
-    def get_version(self, beautify):
-        """Response contains a GitHub release API response and since we request latest the tag_name will correspond
+    def get_version(self, beautify: bool) -> str:
+        """Response contains a GitHub release API response, and since we request latest the tag_name will correspond
         to the actual latest available version"""
         response = requests.get(self.url)
         response.raise_for_status()
@@ -162,12 +159,11 @@ class GithubPrefixReleaseSpider(AbstractSpider):
     """
     Retrieve highest version matching a prefix
     """
-    def __init__(self, owner, repository, prefix):
-        api = 'https://api.github.com/repos/{owner}/{repository}/releases'
-        self.url = api.format(owner=owner, repository=repository)
+    def __init__(self, owner: str, repository: str, prefix: str) -> None:
+        self.url = f'https://api.github.com/repos/{owner}/{repository}/releases'
         self.prefix = prefix
 
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         response = requests.get(self.url)
         response.raise_for_status()
         data = response.json()
@@ -180,11 +176,10 @@ class GithubPrefixReleaseSpider(AbstractSpider):
 
 
 class GithubReleaseSpider(AbstractSpider):
-    def __init__(self, owner, repository):
-        api = 'https://api.github.com/repos/{owner}/{repository}/releases'
-        self.url = api.format(owner=owner, repository=repository)
+    def __init__(self, owner, repository) -> None:
+        self.url = f'https://api.github.com/repos/{owner}/{repository}/releases'
 
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         response = requests.get(self.url)
         response.raise_for_status()
         data = response.json()
@@ -195,12 +190,12 @@ class GithubReleaseSpider(AbstractSpider):
 
 class JenkinsStableSpider(AbstractSpider):
     """
-    Retrieve version for the latest stable Jenkins release (parse the published LTS changelog)
+    Retrieve the version for the latest stable Jenkins release (parse the published LTS changelog)
     """
-    def __init__(self):
+    def __init__(self) -> None:
         self.url = "https://jenkins.io/changelog-stable/"
 
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         """
         XPath version scanner for the Jenkins Stable/LTS change log page
         Usually the version there begins with a v
@@ -216,15 +211,13 @@ class KubernetesVersionLabelSpider(AbstractSpider):
     """
     Retrieve version from a running k8s deployment assuming it's been labeled with app.kubernetes.io/version
     """
-    def __init__(self, item, name, namespace):
+    def __init__(self, item: str, name: str, namespace: str) -> None:
         self.item = item
         self.name = name
         self.namespace = namespace
 
-    def get_version(self, beautify):
-        kubectl_command = "kubectl get {item} {name} -n {namespace} -o yaml".format(
-            item=self.item, name=self.name, namespace=self.namespace
-        )
+    def get_version(self, beautify: bool) -> str:
+        kubectl_command = f"kubectl get {self.item} {self.name} -n {self.namespace} -o yaml"
         result = subprocess.run(kubectl_command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8')
         data = yaml.load(result.stdout, yaml.SafeLoader)
         return _beautify_version(_get_version_from_metadata_label(data), beautify)
@@ -236,16 +229,14 @@ class KubernetesImageVersionSpider(AbstractSpider):
     For a stateful set the pattern could be: spec.template.spec.containers.0.image
     This spider does support numeric indexes used for list items.
     """
-    def __init__(self, item, name, namespace, pattern):
+    def __init__(self, item: str, name: str, namespace: str, pattern: str) -> None:
         self.item = item
         self.name = name
         self.namespace = namespace
         self.pattern = pattern
 
-    def get_version(self, beautify):
-        kubectl_command = "kubectl -n {namespace} get {item} {name} -o yaml".format(
-            namespace=self.namespace, item=self.item, name=self.name
-        )
+    def get_version(self, beautify: bool) -> str:
+        kubectl_command = f"kubectl -n {self.namespace} get {self.item} {self.name} -o yaml"
         result = subprocess.run(kubectl_command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8')
         data = yaml.load(result.stdout, yaml.SafeLoader)
         section = data
@@ -264,10 +255,10 @@ class SonarQubeReleaseSpider(AbstractSpider):
     """
     Retrieves version using SonarQubes download pages (HTML scrape). Hopefully somewhat stable page.
     """
-    def __init__(self):
+    def __init__(self) -> None:
         self.url = 'https://binaries.sonarsource.com/Distribution/sonarqube/'
 
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         """
         XPath version scanner for the SonarQube Distribution page. Finds all downloads for sonarqube and sorts them
         """
@@ -288,15 +279,15 @@ class NASpider(AbstractSpider):
     """
     Very crude N/A spider. Mostly useful while developing
     """
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         return "N/A"
 
 class StaticSpider(AbstractSpider):
     """
     StaticSpider simply returns the version you give as input
     """
-    def __init__(self, version):
+    def __init__(self, version: str) -> None:
         self.version = version
 
-    def get_version(self, beautify):
+    def get_version(self, beautify: bool) -> str:
         return _beautify_version(self.version, beautify)
